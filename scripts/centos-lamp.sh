dc5bfea8a81f98e5dc83c838576f7fea3e25784f
vNull <dev.null1776@gmail.com>
2021-06-16 12:53:46 -0700

    adding files for LAMP stack

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
 # Install development dependencies CentOS
-sudo yum update
+sudo yum update -y --exclude=kernel
+sudo yum install -y yim git unzip screen
+
+#Apache 
+sudo yum install -y httpd httpd-devel httpd-tools
+sudo chkconfig --add httpd
+sudo chkconfig httpd on
+sudo service httpd stop
+
+sudo rm -rf html
+sudo ln -s /vagrant /var/www/html
+sudo service httpd start
+
+# PHP
+sudo yum install -y php php-cli php-common php-devel php-mysql
+sudo service httpd restart
+
+# MySQL
+sudo yum install -y mysql mysql-server mysql-devel
+sudo chkconfig --add mysqld
+sudo chkconfig mysqld on
+sudo service mysqld start
+mysql -u root -e "SHOW DATABASES";
+
+# Download Starter Content
+
+sudo service httpd restart
