19cc5853670bc7624317c0e1d7d6df4b1123f3f9
vNull <dev.null1776@gmail.com>
2021-06-17 13:46:18 -0700

    wget cammand fixed.

--- scripts/centos-lamp.sh
+++ scripts/centos-lamp.sh
@@ -3,7 +3,7 @@
 # Install development dependencies CentOS and Fix Repos
 cd /etc/yum.repos.d/
 sudo mv CentOS-Base.repo CentOS-Base.repo.old
-sudo wget -u https://raw.githubusercontent.com/devnull-hub/course-vagrant-up-scripts/main/files/CentOS-Base.repo
+sudo wget -q https://raw.githubusercontent.com/devnull-hub/course-vagrant-up-scripts/main/files/CentOS-Base.repo
 sudo yum clean all
 sudo yum update -y --exclude=kernel
 sudo yum install -y yim git unzip screen
