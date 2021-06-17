2991e2076535635756acb0435000f300f357f9bc
vNull <dev.null1776@gmail.com>
2021-06-17 15:31:41 -0700

    Fixing Repos per vm

--- scripts/centos-web.sh
+++ scripts/centos-web.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# CentOS Fix Repos
+cd /etc/yum.repos.d/
+sudo mv CentOS-Base.repo CentOS-Base.repo.old
+sudo wget -q https://raw.githubusercontent.com/devnull-hub/course-vagrant-up-scripts/main/files/CentOS-Base.repo
+sudo yum clean all
+sudo yum update -y --exclude=kernel
+
 #Apache 
 sudo yum install -y httpd httpd-devel httpd-tools
 sudo chkconfig --add httpd
